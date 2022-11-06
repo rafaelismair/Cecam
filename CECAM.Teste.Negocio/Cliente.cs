f6d3c8d2fcfc1b54761897e13710175b8c34c5fe
Rafael Ismair Ferreira <rafaelismair@gmail.com>
2022-11-05 23:59:01 -0300

    add camada de negocio

---
+++ CECAM.Teste.Negocio/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Transactions;
+
+namespace CECAM.Teste.Negocio
+{
+    public class Cliente
+    {
+        Dado.Cliente _dadoCliente = null;
+
+        private Dado.Cliente DadoCliente
+        {
+            get
+            {
+                if (_dadoCliente == null)
+                    _dadoCliente = new Dado.Cliente();
+                return _dadoCliente;
+            }
+
+        }
+
+        public void Incluir()
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+
+            }
+        }
+    }
+}
